9ce028916c1cf475d9ec6ff5c627171d0d1e3298
EduardoNassif509 <167436559+EduardoNassif509@users.noreply.github.com>
2024-12-11 22:14:12 -0300

    Cambios en Movil
    Agregada validacion en moviles

--- NeoRemiseria/Models/Movil.cs
+++ NeoRemiseria/Models/Movil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NeoRemiseria.Models;
 
@@ -7,20 +8,26 @@ public partial class Movil
 {
     public uint Id { get; set; }
 
+    [Required(ErrorMessage = "La patente es obligatoria.")]
+    [RegularExpression(@"^([A-Z]{3}[0-9]{3}|[A-Z]{2}[0-9]{3}[A-Z]{2})$", ErrorMessage = "La patente debe estar en formato argentino (ej: ABC123 o AB123CD).")]
     public string Patente { get; set; } = null!;
 
-    public int Habilitacion {get; set;} = 0;
+    public int Habilitacion { get; set; } = 0;
 
+    [Required(ErrorMessage = "El año es obligatorio.")]
+    [Range(2008, 2023, ErrorMessage = "El auto no debe exceder los 15 años de antigüedad.")]
     public int? Año { get; set; }
     // public ushort? Año { get; set; }
 
+    [Required(ErrorMessage = "El modelo es obligatorio.")]
     public uint? IdModelo { get; set; }
 
     public uint? IdCartel { get; set; }
 
+    [Required(ErrorMessage = "La persona titular es obligatoria.")]
     public uint? IdTitular { get; set; }
 
-    public char Estado {get; set;} = 'A';
+    public char Estado { get; set; } = 'A';
 
     public virtual ICollection<Cartel> Carteles { get; set; } = new List<Cartel>();
 
